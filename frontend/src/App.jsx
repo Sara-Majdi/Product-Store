5c09da309b23cbb7de92d085ba6a029e0302e5e1
Sara <maisarah.majdi01@gmail.com>
2024-09-08 14:46:02 +0800

    created routes to pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Box } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import Homepage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <Box minH={"100vh"}>
+      <Navbar /> {/* Put above Routes, cuz every page has Navbar */}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create" element={<createPage />} />
+      </Routes>
+    </Box>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <Container maxW={"1140px"} px={4}>
+        
+    </Container>
+  )
+}
+
+export default Navbar
